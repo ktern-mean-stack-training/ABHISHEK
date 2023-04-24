fb5472d31282343ac627e1d813fad095a59638b5
ABHISHEKGOTHAM1928 <abhishekgotham1289@gmail.com>
2023-04-24 09:36:31 +0530

    add:database as mongo

---
+++ nodejs/day-2/api.js
@@ -0,0 +1,126 @@
+//=============================DAY-02 ASSIGNMENT============================
+//refer notes.txt for clarification
+const express =require("express");
+const app = express();
+app.use(express.json())
+
+
+const userModel = require("./mongoapi");   // importing the mongodb skeleton
+
+//1. Adding userdetails into mongodb database --DONE
+app.post("/register", async (req,res)=>{        
+    dbUrl = "mongodb://localhost:27017"  //url of the database
+    dbName = "userdb"                    //name of the db working on
+    const userdetails = req.body;       // variable to assign the user input
+    const addUser = await userModel.addUserDetails(dbUrl, dbName, userdetails) //variable to call the required function 
+                                                                //(moves to mongo file, with parsing url and name of the 
+                                                                // db, along with user input)
+
+    res.send({                  //sending the res to the client
+        // addUser,
+        msg:"student register successfully"
+    
+    })
+})
+
+//2. updating userdetails of mongodb --DONE
+app.put("/update/:id", async(req,res)=>{
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+    const userId = req.params.id;
+
+    const updateData = req.body
+
+    const updateUser = await userModel.updateUserDetails(dbUrl,dbName,userId, updateData)
+
+    res.send({msg: "updated successfully"});
+
+
+})
+
+//3. delete the student entry --DONE
+
+app.delete ("/delete/:id",async(req,res)=>{
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+    const userId = req.params.id;
+    const deleteUser = await userModel.deleteUserDetails(dbUrl,dbName,userId)
+
+    res.send({
+        msg: `User with ID ${userId} deleted successfully`,
+      });
+
+
+})
+
+//4. retrieving a single student --DONE
+
+app.get("/student_id/:id", async(req,res)=>{
+    const userId = req.params.id;
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+    
+    const findUser = await userModel.singleUserDetails(dbUrl, dbName, userId)
+
+    res.send({
+        findUser
+        
+    })
+
+})
+
+//5. retrieving all the data --DONE
+
+app.get("/students", async(req,res)=>{
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+
+    const findeUsers = await userModel.allUserDetails(dbUrl, dbName)
+
+    res.send({
+        findeUsers
+    })
+})
+
+// 6. getting the percentage --Done
+
+app.get("/percentage", async(req,res)=>{
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+
+    const percentage_Users = await userModel.getPercentage(dbUrl,dbName)
+
+    res.send({percentage_Users})
+})
+
+// 7. Eligible percentage --DONE
+
+app.get("/eligible", async(req,res)=>{
+    dbUrl = "mongodb://localhost:27017"
+    dbName = "userdb"
+
+    const EligibleStudents = await userModel.getEligible(dbUrl,dbName)
+
+    res.send({EligibleStudents})
+
+
+})
+
+// 8. CUTOFF --
+app.get("/cutoff", async(req,res)=>{
+    dbUrl ="mongodb://localhost:27017"
+    dbName="userdb"
+
+    const Cutoff = await userModel.cutOff(dbUrl,dbName)
+
+    res.send({Cutoff})
+})
+
+
+//================================================
+
+
+
+app.listen(4000, () => {
+    console.log("app running on port 4000");
+  });
--- nodejs/day-2/mongoapi.js
+++ nodejs/day-2/mongoapi.js
@@ -1,1 +1,169 @@
 //mongo db
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+
+//to add or register students
+module.exports.addUserDetails = async(url,dbName, userData)=>  {   //exporting the module that has arguments that are shared via calling.
+    const client = await MongoClient.connect(url)  //creating a promise and waiting for the client to respond
+    const addUser = client  //variable to perform the function
+        .db(dbName)   // name of the db [shared via arguments]
+        .collection("users")  //name of the collection in the db
+        .insertOne(userData)    //inserOne: used to insert a data in the db
+
+    return addUser;  //returning the above variable
+ 
+}
+
+//to update students
+
+module.exports.updateUserDetails= async(url,dbName, userId, updateData)=>{
+    const client = await MongoClient.connect(url)
+    const updateUser = client
+        .db(dbName)
+        .collection("users")
+        .updateOne({_id: new ObjectId(userId)}, { $set: updateData }); 
+
+    return updateUser;
+
+}
+
+// to delete students
+
+module.exports.deleteUserDetails = async(url, dbName, userId)=>{
+    const client = await MongoClient.connect(url)
+    const deleteUser = await client
+        .db(dbName)
+        .collection("users")
+        .deleteOne({_id: new ObjectId(userId)})
+
+        client.close();
+}
+
+// to retrieve single student
+
+module.exports.singleUserDetails = async (url, dbName, userId)=> {
+    const client = await MongoClient.connect(url)
+    const findUser = await client
+        .db(dbName)
+        .collection("users")
+        .findOne({_id : new ObjectId(userId)})
+
+        // client.close();
+
+        return findUser
+}
+
+// to retrive all the students
+
+module.exports.allUserDetails = async(url, dbName,) => {
+    const client = await MongoClient.connect(url)
+    const findUser = await client
+        .db(dbName)
+        .collection("users")
+        .find()
+        .toArray()
+
+    return findUser
+}
+
+// to get percentage
+
+module.exports.getPercentage = async(url, dbName) => {
+    const client = await MongoClient.connect(url)
+    const percentage = await client
+        .db(dbName)
+        .collection("users")
+        .aggregate([
+            {
+                $project:{
+
+                    name: 1,
+                    maths: 1,
+                    physics:1,
+                    chemistry:1,
+                    english:1,
+                    
+                    
+                    percentage: {$divide: [{$add: ["$maths","$physics","$chemistry","$english"]},4]}
+                    
+                }
+            }
+
+
+        ])
+        .toArray()
+        
+        return percentage
+
+}
+
+// to get students with >80 percentage
+
+module.exports.getEligible = async(url, dbName)=>{
+    const client = await MongoClient.connect(url)
+    const percentage = await client
+        .db(dbName)
+        .collection("users")
+        .aggregate([
+            {
+                $project:{
+                    _id:1,
+                    name: 1,
+                    maths: 1,
+                    physics:1,
+                    chemistry:1,
+                    english:1,
+                    
+                    percentage: {$divide: [{$add: ["$maths","$physics","$chemistry","$english"]},4]}
+                }
+            },
+
+            {
+                $match:{
+                    percentage: {$gt: 80}
+                }
+            }
+
+        ])
+        .toArray()
+
+        return percentage
+}
+
+module.exports.cutOff = async(url,dbName)=>{
+    const client = await MongoClient.connect(url)
+    const cutoff = await client
+        .db(dbName)
+        .collection("users")
+        .aggregate([
+            {
+                $project:{
+                    name: 1,
+                    maths: 1,
+                    physics:1,
+                    chemistry:1,
+                    english:1,
+                    
+                    // cutoff: {$add:[{$divide:["$maths",0.5]},{$divide:["$physics",0.25]},{$divide:["$chemistry",0.25]}]}
+                    cutoff: {$divide:[{$add:[{$divide:["$maths",0.5]},{$divide:["$physics",0.25]},{$divide:["$chemistry",0.25]}]},10]}
+
+                    
+                }
+            },
+            {
+                $match:{
+                    cutoff: {$gt: 80}
+                }
+            
+
+            }
+        ])
+        .toArray()
+
+        return cutoff
+}
+
+
+
+
